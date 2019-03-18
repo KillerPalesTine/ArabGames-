e9c0adea6bdb85eac414c0e9b9f9000602939433
KillerPalesTine <46435288+KillerPalesTine@users.noreply.github.com>
2019-03-18 15:28:04 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
  
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-   client.user.setActivity("m%help |ArabGamesMusic",{type: 'Streaming'})
+   client.user.setGame(`%help | ArabGames`,"http://twitch.tv/Death Shop")
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
