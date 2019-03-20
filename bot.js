3c304f9f738bd698203b7c179938b0d40f22eeb8
KillerPalesTine <46435288+KillerPalesTine@users.noreply.github.com>
2019-03-20 14:47:05 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,321 +1,217 @@
-const moment = require('moment')
-const ytdl = require("ytdl-core");
-const { Client, Util } = require('discord.js');
-const getYoutubeID = require('get-youtube-id');
-const fetchVideoInfo = require('youtube-info');
-const YouTube = require('simple-youtube-api');
-const youtube = new YouTube("AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8");//youtube api key
-const queue = new Map();//the queue map
-const token = process.env.BOT_TOKEN//your bot token
-const prefix = 'm%'//your prefix
+﻿const Discord = require("discord.js");
+const client = new Discord.Client();
+const prefix = "$";
+ 
 
 
-//All copytights for Eyad & (old) Codes
-
-/*
-Packages
-npm install discord.js
-npm install ytdl-core
-npm install get-youtube-id
-npm install youtube-info
-npm install simple-youtube-api
-npm install queue
-*/
-
-client.on('ready', () => {//the ready msgs
-    console.log(`Logged in as ${client.user.tag}!`);
-    console.log(`in ${client.guilds.size} servers `)
-    console.log(`[ ] ${client.users.size}`)
-client.user.setGame(`m%play | ArabGames`,"http://twitch.tv/Death Shop")
+client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(`KillerPalesTine`);
+        console.log(`----------------`);
+      console.log(`ON ${client.guilds.size} Servers '     Script By : ArabGames ' `);
+    console.log(`----------------`);
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`$help | ArabGames`,"http://twitch.tv/Death Shop")
+client.user.setStatus("online")
 });
 
-client.on('message', async msg => {
-	if (msg.author.bot) return undefined;
-	if (!msg.content.startsWith(prefix)) return undefined;
-	const args = msg.content.split(' ');
-	const searchString = args.slice(1).join(' ');
-	const url = args[1] ? args[1] .replace(/<(.+)>/g, '$1') : '';
-	const serverQueue = queue.get(msg.guild.id);
-	let command = msg.content.toLowerCase().split(" ")[0];
-	command = command.slice(prefix.length)
-	if (command === `play`) {
-		const voiceChannel = msg.member.voiceChannel;
-		if (!voiceChannel) return msg.channel.send('يجب توآجد حضرتك بروم صوتي .');
-		const permissions = voiceChannel.permissionsFor(msg.client.user);
-		if (!permissions.has('CONNECT')) {
-			return msg.channel.send('لا يتوآجد لدي صلاحية للتكلم بهذآ الروم');
-		}
-		if (!permissions.has('SPEAK')) {
-			return msg.channel.send('لا يتوآجد لدي صلاحية للتكلم بهذآ الروم');
-		}
 
-		if (!permissions.has('EMBED_LINKS')) {
-			return msg.channel.sendMessage("**يجب توآفر برمشن `EMBED LINKS`لدي **rl")
-			}
+client.on('message', puz => {
+    if (puz.content == "$ألعاب") {//By Lion_Dz
+        var x = ["https://f.top4top.net/p_1169dw8521.png",
+"https://e.top4top.net/p_1169in6a31.png",
+"https://f.top4top.net/p_1169ee7gw2.png",
+"https://a.top4top.net/p_1169mgboh3.png",
+"https://b.top4top.net/p_1169q1cq14.png",
+"https://c.top4top.net/p_1169clnyc5.png",
+"https://d.top4top.net/p_1169hpgrq6.png",
+"https://e.top4top.net/p_116906rsa7.png",
+"https://f.top4top.net/p_1169ije9g8.png",
+"https://a.top4top.net/p_1169wt3369.png",
+];
+        var x2 = ['blur',
+        "Creative Destruction",
+        "Fortnite",
+        "Gta IV",
+        "Gta V",
+        "Gta Sa",
+        "Minecraft",
+        "Outlast",
+        "Outlast2",
+        "Residentevil7",
+       
+       
+       
+       
+        ];
+       
+        var x3 = Math.floor(Math.random()*x.length)
+        puz.channel.send(`السؤال هو:  __**${x[x3]}**__
+**لديك 15 ثانية للإجابة**`).then(msg1=> {
+            var r = puz.channel.awaitMessages(msg => msg.content == x2[x3], {
+                maxMatches : 1,
+                time : 15000,
+                errors : ['time']
+            })
+        r.catch(() => {
+            return puz.channel.send(`:negative_squared_cross_mark: **لقد انتهى الوقت ولم يقم أحد بالأجابة بشكل صحيح**
+            `)
+        })
+       
+        r.then((collected)=> {
+            puz.channel.send(`${collected.first().author} **إجابة صحيحة** :white_check_mark:   `);
+        })
+        })
+    }
+})
 
-		if (url.match(/^https?:\/\/(www.youtube.com|youtube.com)\/playlist(.*)$/)) {
-			const playlist = await youtube.getPlaylist(url);
-			const videos = await playlist.getVideos();
-			for (const video of Object.values(videos)) {
-				const video2 = await youtube.getVideoByID(video.id);
-				await handleVideo(video2, msg, voiceChannel, true);
-			}
-			return msg.channel.send(` **${playlist.title}** تم الإضآفة إلى قأئمة التشغيل`);
-		} else {
-			try {
 
-				var video = await youtube.getVideo(url);
-
-			} catch (error) {
-				try {
-				    					    var fast = {};
-					var videos = await youtube.searchVideos(searchString, 10);
-					let index = 0;
-					const embed1 = new Discord.RichEmbed()
-			        .setDescription(`**الرجآء من حضرتك إختيآر رقم المقطع** :
-${videos.map(video2 => `[**${++index}**] **${video2.title}**`).join('\n')}`)
-					.setFooter(`${msg.guild.name}`)
-					msg.channel.sendEmbed(embed1).then(message =>{
-
-						message.delete(15000)
-
-					});
-					try {
-						var response = await msg.channel.awaitMessages(msg2 => msg2.content > 0 && msg2.content < 11, {
-							maxMatches: 1,
-							time: 20000,
-							errors: ['time']
-						})
-
-						}catch(err) {
-						console.error(err);
-						return msg.channel.send('لم يتم إختيآر مقطع صوتي');
-						}
-					const videoIndex = parseInt(response.first().content);
-					var video = await youtube.getVideoByID(videos[videoIndex - 1].id);
-				} catch (err) {
-					console.error(err);
-					return msg.channel.send(':x: لا يتوفر نتآئج بحث ');
-				}
-		}
-
-			return handleVideo(video, msg, voiceChannel);
-		}
-	} else if (command === `skip`) {
-		if (!msg.member.voiceChannel) return msg.channel.send('أنت لست بروم صوتي .');
-		if (!serverQueue) return msg.channel.send('لا يتوفر مقطع لتجآوزه');
-		serverQueue.connection.dispatcher.end('تم تجآوز هذآ المقطع');
-		return undefined;
-	} else if (command === `stop`) {
-		if (!msg.member.voiceChannel) return msg.channel.send('أنت لست بروم صوتي .');
-		if (!serverQueue) return msg.channel.send('لا يتوفر مقطع لإيقآفه');
-		serverQueue.songs = [];
-		serverQueue.connection.dispatcher.end('تم إيقآف هذآ المقطع');
-		return undefined;
-	} else if (command === `vol`) {
-		if (!msg.member.voiceChannel) return msg.channel.send('أنت لست بروم صوتي .');
-		if (!serverQueue) return msg.channel.send('لا يوجد شيء شغآل.');
-		if (!args[1]) return msg.channel.send(`:loud_sound: مستوى الصوت **${serverQueue.volume}**`);
-		serverQueue.volume = args[1];
-		serverQueue.connection.dispatcher.setVolumeLogarithmic(args[1] / 50);
-		return msg.channel.send(`:speaker: تم تغير الصوت الي **${args[1]}**`);
-	} else if (command === `np`) {
-		if (!serverQueue) return msg.channel.send('لا يوجد شيء حالي ف العمل.');
-		const embedNP = new Discord.RichEmbed()
-	.setDescription(`:notes: الان يتم تشغيل : **${serverQueue.songs[0].title}**`)
-		return msg.channel.sendEmbed(embedNP);
-	} else if (command === `replay`) {
-		if (!serverQueue) return msg.channel.send('لا يوجد شيء حالي ف العمل.');
-		const embedNP = new Discord.RichEmbed()
-	.setDescription(`سيتم اعاده تشغيل الفديو :**${serverQueue.songs[0].title}**`)
-	msg.channel.send({embed: embedNP})
-  	 return handleVideo(video, msg, msg.member.voiceChannel);
-
-	} else if (command === `queue`) {
-		if (!serverQueue) return msg.channel.send('لا يوجد شيء حالي ف العمل.');
-		let index = 0;
-		const embedqu = new Discord.RichEmbed()
-.setDescription(`**Songs Queue**
-${serverQueue.songs.map(song => `**${++index} -** ${song.title}`).join('\n')}
-**الان يتم تشغيل** ${serverQueue.songs[0].title}`)
-		return msg.channel.sendEmbed(embedqu);
-	} else if (command === `pause`) {
-		if (serverQueue && serverQueue.playing) {
-			serverQueue.playing = false;
-			serverQueue.connection.dispatcher.pause();
-			return msg.channel.send('تم إيقاف الموسيقى مؤقتا!');
-		}
-		return msg.channel.send('لا يوجد شيء حالي ف العمل.');
-	} else if (command === "resume") {
-		if (serverQueue && !serverQueue.playing) {
-			serverQueue.playing = true;
-			serverQueue.connection.dispatcher.resume();
-			return msg.channel.send('استأنفت الموسيقى بالنسبة لك !');
-		}
-		return msg.channel.send('لا يوجد شيء حالي في العمل.');
-	}
-
-	return undefined;
-async function handleVideo(video, msg, voiceChannel, playlist = false) {
-	const serverQueue = queue.get(msg.guild.id);
-	const song = {
-		id: video.id,
-		title: Util.escapeMarkdown(video.title),
-		url: `https://www.youtube.com/watch?v=${video.id}`,
-		time:`${video.duration.hours}:${video.duration.minutes}:${video.duration.seconds}`,
-		eyad:`${video.thumbnails.high.url}`,
-		best:`${video.channel.title}`,
-		bees:`${video.raw.snippet.publishedAt}`,
-		shahd:`${video.raw.kind}`,
-		zg:`${video.raw.snippet.channelId}`,
-        views:`${video.raw.views}`,
-        like:`${video.raw.likeCount}`,
-        dislike:`${video.raw.dislikeCount}`,
-        hi:`${video.raw.id}`
-	};
-	if (!serverQueue) {
-		const queueConstruct = {
-			textChannel: msg.channel,
-			voiceChannel: voiceChannel,
-			connection: null,
-			songs: [],
-			volume: 5,
-			playing: true
-		};
-		queue.set(msg.guild.id, queueConstruct);
-		queueConstruct.songs.push(song);
-		try {
-			var connection = await voiceChannel.join();
-			queueConstruct.connection = connection;
-			play(msg.guild, queueConstruct.songs[0]);
-		} catch (error) {
-			console.error(`I could not join the voice channel: ${error}`);
-			queue.delete(msg.guild.id);
-			return msg.channel.send(`لا أستطيع دخول هذآ الروم ${error}`);
-		}
-	} else {
-		serverQueue.songs.push(song);
-		console.log(serverQueue.songs);
-		if (playlist) return undefined;
-		else return msg.channel.send(` **${song.title}** تم اضافه الاغنية الي القائمة!`);
-	}
-	return undefined;
-}
-
-function play(guild, song) {
-	const serverQueue = queue.get(guild.id);
-
-	if (!song) {
-		serverQueue.voiceChannel.leave();
-		queue.delete(guild.id);
-		return;
-	}
-	console.log(serverQueue.songs);
-	const dispatcher = serverQueue.connection.playStream(ytdl(song.url))
-		.on('end', reason => {
-			if (reason === 'Stream is not generating quickly enough.') console.log('Song ended.');
-			else console.log(reason);
-			serverQueue.songs.shift();
-			play(guild, serverQueue.songs[0]);
-		})
-		.on('error', error => console.error(error));
-	dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
-		fetchVideoInfo(`${song.hi}`, function (err,  idk) {
-  if (err) throw new Error(err);
-  console.log( idk);
-  	  const yyyy = {}
-  if(!yyyy[msg.guild.id]) yyyy[msg.guild.id] = {
-    like: `${ idk.likeCount}`,
-    dislike: `${ idk.dislikeCount}`
-  }
-	serverQueue.textChannel.send({embed : new Discord.RichEmbed()
-  .setTitle(`**${ idk.title}**`)
-  .setURL( idk.url)
-  .addField('Time The Video :' , `${song.time}`, true)
-  .addField('Channel Name :' , `${song.best}`, true)
-  .addField('Channel ID :' , `${song.zg}`, true)
-  .addField('Video Created at :' , `${ idk.datePublished}`, true)
-  .addField('Views :' , `${ idk.views}`, true)
-  .addField('Like👍 :' , `${ idk.likeCount}`, true)
-  .addField('dislike👎 :' , `${ idk.dislikeCount}`, true)
-  .addField('comments :' , `${ idk.commentCount}`, true)
-	.setImage(`${song.eyad}`)
-	.setThumbnail('http://cdn.akhbaar24.com/430e061a-f89a-43c7-86d9-82fae5f7c495.jpg')
-	.setColor('#ff0000')
-	.setTimestamp()
-	}).then(love => {
-		love.react('👍').then(r=>{
-		love.react('👎').then(r =>{
-		love.react('🙌').then(r=> {
-    let likee = (reaction, user) => reaction.emoji.name === '👍' && user.id === msg.author.id;
-    let dislikee = (reaction, user) => reaction.emoji.name === '👎' && user.id === msg.author.id;
-    let cnn = (reaction, user) => reaction.emoji.name === '🙌' && user.id === msg.author.id;
-
-    let ll = love.createReactionCollector(likee , {max:5});
-    let dd = love.createReactionCollector(dislikee , {max:5});
-    let cn = love.createReactionCollector(cnn , {max:5});
-
-		    ll.on("collect", r => {
-		      yyyy[msg.guild.id].like++;
-	love.edit({embed : new Discord.RichEmbed()
-  .setTitle(`**${ idk.title}**`)
-  .setURL( idk.url)
-  .addField('Time The Video :' , `${song.time}`, true)
-  .addField('Channel Name :' , `${song.best}`, true)
-  .addField('Channel ID :' , `${song.zg}`, true)
-  .addField('Video Created at :' , `${ idk.datePublished}`, true)
-  .addField('Views :' , `${ idk.views}`, true)
-  .addField('Like👍 :' , `${yyyy[msg.guild.id].like}`, true)
-  .addField('dislike👎 :' , `${ idk.dislikeCount}`, true)
-  .addField('comments :' , `${ idk.commentCount}`, true)
-	.setImage(`${song.eyad}`)
-	.setThumbnail('http://cdn.akhbaar24.com/430e061a-f89a-43c7-86d9-82fae5f7c495.jpg')
-	.setColor('#ff0000')
-	.setTimestamp()
+client.on('message', message => {
+    if (message.content == "$سرعة") {
+        var x = ["https://cdn.discordapp.com/attachments/553287876243357721/553515081296773140/bandicam_2019-03-07_12-50-04-444.jpg",
+"https://cdn.discordapp.com/attachments/553287876243357721/553515077697929241/bandicam_2019-03-07_12-45-37-573.jpg",
+"https://cdn.discordapp.com/attachments/553287876243357721/553515078180405249/bandicam_2019-03-07_12-48-29-284.jpg",
+"https://cdn.discordapp.com/attachments/553287876243357721/553515078515949568/bandicam_2019-03-07_12-48-07-648.jpg",
+"https://cdn.discordapp.com/attachments/553287876243357721/553515078129942548/bandicam_2019-03-07_12-49-15-371.jpg",
+"https://cdn.discordapp.com/attachments/553287876243357721/553515077458853898/bandicam_2019-03-07_12-48-44-988.jpg",
+"https://cdn.discordapp.com/attachments/553287876243357721/553515076385112065/bandicam_2019-03-07_12-47-45-547.jpg",
+"https://cdn.discordapp.com/attachments/553287876243357721/553515076162813963/bandicam_2019-03-07_12-47-11-464.jpg",
+"https://cdn.discordapp.com/attachments/553287876243357721/553515073897889793/bandicam_2019-03-07_12-46-28-283.jpg",
+"https://cdn.discordapp.com/attachments/553287876243357721/553515071716982806/bandicam_2019-03-07_12-44-55-900.jpg",
+"https://cdn.discordapp.com/attachments/553287876243357721/553515050653188097/bandicam_2019-03-07_12-44-33-759.jpg",
+"https://cdn.discordapp.com/attachments/553287876243357721/553515045334810656/bandicam_2019-03-07_12-44-15-396.jpg",
+"https://cdn.discordapp.com/attachments/553287876243357721/553515038917525505/bandicam_2019-03-07_12-43-48-900.jpg",
+"https://cdn.discordapp.com/attachments/553287876243357721/553515019153965066/bandicam_2019-03-07_12-43-09-939.jpg",
+"https://cdn.discordapp.com/attachments/553287876243357721/553515014066012160/bandicam_2019-03-07_12-42-33-352.jpg",
+];
+        var x2 = ['By Killer',
+        "طبق رطب مرق بقر",
+        "عش العصفور",
+        "شجرة الأوغيري",
+"ليوبليانا",
+"كانيكي",
+"خير و طماطم",
+"تفاح و زلاطه",
+"خيار وطرشي",
+"جافاسكربت",
+"امازون",
+"النهاية",
+"القسطنطينية",
+"السعودية",
+"البرازيل",
+     ];
+       
+        var x3 = Math.floor(Math.random()*x.length)
+        message.channel.send(`  اكتب اسرع
+ :  ${x[x3]}
+لديك 20 ثانية`).then(msg1=> {
+            var r = message.channel.awaitMessages(msg => msg.content == x2[x3], {
+                maxMatches : 1,
+                time : 20000,
+                errors : ['time']
+            })
+        r.catch(() => {
+            return message.channel.send(`:negative_squared_cross_mark: لقد انتهى الوقت ولم يقم أحد بالأجابة بشكل صحيح
+            الإجآبة الصحيحةة هي __**${x2[x3]}**__`)
+        })
+       
+        r.then((collected)=> {
+            message.channel.send(`${collected.first().author}لقد قمت بكتابة الكلمه في الوقت المناسب`);
+        })
+        })
+    }
 });
-    })
 
-    dd.on("collect", r => {
-      yyyy[msg.guild.id].dislike++;
-	love.edit({embed : new Discord.RichEmbed()
-  .setTitle(`**${ idk.title}**`)
-  .setURL( idk.url)
-  .addField('Time The Video :' , `${song.time}`, true)
-  .addField('Channel Name :' , `${song.best}`, true)
-  .addField('Channel ID :' , `${song.zg}`, true)
-  .addField('Video Created at :' , `${ idk.datePublished}`, true)
-  .addField('Views :' , `${ idk.views}`, true)
-  .addField('Like👍 :' , `${ idk.likeCount}`, true)
-  .addField('dislike👎 :' , `${yyyy[msg.guild.id].dislike}`, true)
-  .addField('comments :' , `${ idk.commentCount}`, true)
-	.setImage(`${song.eyad}`)
-	.setThumbnail('http://cdn.akhbaar24.com/430e061a-f89a-43c7-86d9-82fae5f7c495.jpg')
-	.setColor('#ff0000')
-	.setTimestamp()
+
+client.on('message', fkk => {
+    if (fkk.content == "$برنامج") {
+        var x = ["المتاح للجميع لا يتاح لي",
+"https://b.top4top.net/p_1164oe5tp1.jpg",
+"https://c.top4top.net/p_1164jqp402.png",
+"https://d.top4top.net/p_1164kjqt13.jpg",
+"https://e.top4top.net/p_1164qljfs4.png",
+"https://f.top4top.net/p_1164y0w3x5.jpg",
+"https://e.top4top.net/p_1164r89w31.png",
+"https://a.top4top.net/p_1164op08s3.jpg",
+"https://b.top4top.net/p_11641d5sl4.png",
+"https://c.top4top.net/p_1164u1nmr5.jpg",
+"https://b.top4top.net/p_1164eph0n1.jpg",
+"https://c.top4top.net/p_1164uj2802.jpg",
+"https://d.top4top.net/p_116466p0l3.jpg",
+"https://e.top4top.net/p_1164vosgi4.jpg",
+"https://f.top4top.net/p_11649i6xv5.jpg",
+"https://a.top4top.net/p_1164sozlg6.png",
+];
+        var x2 = ['',
+		"فوتوشوب",
+		"ديسكورد",
+		"برو بوت",
+		"فيس بوك",
+		"ثعلب النار",
+		"فايلزيلا",
+		"باندي كام",
+		"ميوزكلى",
+		"يوتيوب",
+		"كلاش واف كلانس",
+		"واتس اب",
+		"ماسنجر",
+		"انستقرام",
+		"جوجل",
+		"تويتر",
+        
+        
+        
+        ];
+        
+        var x3 = Math.floor(Math.random()*x.length)
+        fkk.channel.send(`╔════════【۩ **__برنامج__** ۩】════════╗
+
+***${x[x3]}***
+
+=====================================
+
+**__لديك 15 ثانية للاجابة __**
+╚═════════════════════════════╝`).then(msg1=> {
+            var r = fkk.channel.awaitMessages(msg => msg.content == x2[x3], {
+                maxMatches : 1,
+                time : 15000,
+                errors : ['https://cdn.discordapp.com/attachments/429330153735454722/430040652542246912/bbcb4aa9853bf1d2.png']
+            })
+        r.catch(() => {
+            return fkk.channel.send(`:negative_squared_cross_mark: لقد انتهى الوقت ولم يقم أحد بالأجابة بشكل صحيح 
+            الإجآبة الصحيحةة هي __**${x2[x3]}**__`)
+        })
+        
+        r.then((collected)=> {
+            fkk.channel.send(`${collected.first().author} اجايه صحيح :white_check_mark: `);
+        })
+        })
+    }
+})
+
+
+client.on('message',async message => {
+const emojis   = { arrow: `${client.guilds.find(r => r.id === '553508691425361940').emojis.find(e => e.name === 'arrow')}` };
+
+       if (message.content.startsWith(prefix + "help")) {
+        
+       message.channel.send(`** • قـــائمة الاومر ** :
+       ${emojis.arrow}, **| ${prefix}help-games ⇏ \`$سرعة!\`
+       ${emojis.arrow}, **| ${prefix}help-games ⇏ \`$برنامج!\`
+        ${emojis.arrow},  | ${prefix}help-games ⇏ \`ألعاب!\`** `)
+
+
+   }
+
 });
-})
-    cn.on("collect", r => {
-	love.edit({embed : new Discord.RichEmbed()
-  .setTitle(`**${ idk.title}**`)
-  .setURL( idk.url)
-  .addField('Time The Video :' , `${song.time}`, true)
-  .addField('Channel Name :' , `${song.best}`, true)
-  .addField('Channel ID :' , `${song.zg}`, true)
-  .addField('Video Created at :' , `${ idk.datePublished}`, true)
-  .addField('Views :' , `${ idk.views}`, true)
-  .addField('Like👍 :' , `${ idk.likeCount}`, true)
-  .addField('dislike👎 :' , `${ idk.dislikeCount}`, true)
-  .addField('comments :' , `${ idk.commentCount}`, true)
-	.setImage(`${song.eyad}`)
-	.setThumbnail('http://cdn.akhbaar24.com/430e061a-f89a-43c7-86d9-82fae5f7c495.jpg')
-	.setColor('#ff0000')
-	.setTimestamp()
-});
-})
-})
-})
-})
-})
-})
-}
-});
+
+
+
+
+ 
+ 
+ 
+ 
+ 
+
+client.login(process.env.BOT_TOKEN);
